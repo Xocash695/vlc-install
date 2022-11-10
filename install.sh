856e1720ce4ddc54688f6357df60b3f9691f2500
Xocash695 <akash.kallumkal@gmail.com>
2022-11-10 18:39:44 -0500

    First commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+/bin/bash
+sudo apt-add-repository non-free
+sudo apt-add-repository contrib
+sudo apt -y update 
+sudo apt -y upgrade 
+sudo apt install vlc
+sudo apt -y install libavcodec-extra
+sudo apt -y  install libdvdread4 libdvdcss2
+
