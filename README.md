ab5956753bedd76d6a5cee49dbbff222ba73b643
Xocash695 <akash.kallumkal@gmail.com>
2022-11-10 20:46:28 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,10 @@
+#VLC install 
+
+This project makes easy for people to install VLC and codecs without having to know the commands to enter on a chromebook. 
+
+Instructions: 
+
+Run the command below and vlc will be installed.
+``` 
+curl -s -L https://raw.githubusercontent.com/Xocash695/vlc-install/main/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-/bin/bash
+#!/bin/bash
+sudo apt -y install software-properties-common
 sudo apt-add-repository non-free
 sudo apt-add-repository contrib
 sudo apt -y update 
 sudo apt -y upgrade 
-sudo apt install vlc
+sudo apt -y install vlc
 sudo apt -y install libavcodec-extra
 sudo apt -y  install libdvdread4 libdvdcss2
 
